2a01b4702dc343eb1bba3af0104098df8aedb191
Craig Lucksted <luckstedcraig@gmail.com>
2025-01-22 22:27:16 -0600

    added rotation

--- main.py
+++ main.py
@@ -16,8 +16,10 @@ def main():
 
         screen.fill("black")
         hero.draw(screen)
+        hero.update(dt)
         pygame.display.flip()
-        dt = clock.tick(60)/100
+        dt = clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
--- player.py
+++ player.py
@@ -23,6 +23,17 @@ class Player(circleshape.CircleShape):
             2,
         )
 
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(-dt)
 
 
 
